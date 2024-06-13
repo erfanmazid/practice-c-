7bdb2a77985ce7d26f38e075d05963b92f072b87
erfan mazidabadi <erfanmazidam@gmail.com>
2024-06-13 22:07:08 +0330

    add shopping lists

--- question3/question3.cpp
+++ question3/question3.cpp
@@ -2,3 +2,23 @@
 #include <fstream>
 
 using namespace std;
+
+class shopping
+{
+private:
+    int pid;
+    string name;
+    float price;
+    float dis;
+    int numbers;
+
+public:
+    void menu();
+    void administrator();
+    void buyer();
+    void add();
+    void remove();
+    void edit();
+    void list();
+    void receipt();
+};
