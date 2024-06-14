0ee9f9a953c03b4aee1347854686b7a3f0156331
erfan mazidabadi <erfanmazidam@gmail.com>
2024-06-14 18:48:50 +0330

    add class for product and panel

--- question3/question3.cpp
+++ question3/question3.cpp
@@ -2,3 +2,23 @@
 #include <fstream>
 
 using namespace std;
+
+class shopping
+{
+private:
+    int pId;
+    string pName;
+    float pPrice;
+    float pNumbers;
+
+public:
+    void menu();
+    void adminPanel();
+    void invoicing();
+    void inventory();
+    void addP();
+    void editP();
+    void removeP();
+    void list();
+    void receipt();
+};
