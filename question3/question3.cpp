1a9bdf4121963f4c93e05a92f1ebef16d753ee60
erfan mazidabadi <erfanmazidam@gmail.com>
2024-06-14 21:57:51 +0330

    add add to admin panel

--- question3/question3.cpp
+++ question3/question3.cpp
@@ -9,7 +9,7 @@ private:
     int pId;
     string pName;
     float pPrice;
-    float pNumbers;
+    int pNumbers;
 
 public:
     void menu();
@@ -56,7 +56,7 @@ m:
         cout << "\t\t\t\t  Enter password: \n";
         cin >> password;
 
-        if (email = "erfanmazidam@gmail.com" &&password = "qwer@1384")
+        if (email == "erfanmazidam@gmail.com" && password == "qwer@1384")
         {
             adminPanel();
         }
@@ -75,7 +75,6 @@ m:
 
     default:
         cout << "please select from options!";
-        break;
     };
 
     goto m;
@@ -124,10 +123,99 @@ m:
         break;
 
     default:
-        cout
-            << "please select from options!";
-        break;
+        cout << "please select from options!";
     };
 
     goto m;
+};
+
+void shopping::invoicing()
+{
+m:
+    int choise;
+    cout << "\t\t\t\t-------------------------------\n";
+    cout << "\t\t\t\t                               \n";
+    cout << "\t\t\t\t       Ordering system         \n";
+    cout << "\t\t\t\t                               \n";
+    cout << "\t\t\t\t    1) New order               \n";
+    cout << "\t\t\t\t                               \n";
+    cout << "\t\t\t\t    2) GO back                 \n";
+    cout << "\t\t\t\t                               \n";
+    cout << "\t\t\t\t-------------------------------\n";
+    cout << "\t\t\t\t    Enter your choice:         \n";
+    cin >> choise;
+
+    switch (choise)
+    {
+    case 1:
+        receipt();
+        break;
+
+    case 2:
+        menu();
+        break;
+
+    default:
+        cout << "please select from options!";
+    };
+
+    goto m;
+};
+
+void shopping ::addP()
+{
+m:
+    fstream data;
+    int i;
+    int token = 0;
+    string n;
+    float p;
+    int c;
+
+    cout << "\t\t\t\t                               \n";
+    cout << "\t\t\t\t       Add new product         \n";
+    cout << "\t\t\t\t                               \n";
+    cout << "\t\t\t\t    enter a product id:        \n";
+    cin >> pId;
+    cout << "\t\t\t\t    Enter a name for product   \n";
+    cin >> pName;
+    cout << "\t\t\t\t    Enter a price for product  \n";
+    cin >> pPrice;
+    cout << "\t\t\t\t    Enter a number for product \n";
+    cin >> pNumbers;
+
+    data.open("database.txt", ios::in);
+
+    if (!data)
+    {
+        data.open("database.txt", ios::app | ios::out);
+        data << " " << pId << " " << pName << " " << pPrice << " " << pNumbers << "\n";
+        data.close();
+    }
+    else
+    {
+        data >> i >> n >> p >> c;
+
+        while (data.eof())
+        {
+            if (i == pId)
+            {
+                token++;
+            };
+            data >> i >> n >> p >> c;
+        };
+        data.close();
+
+        if (token == 1)
+        {
+            goto m;
+        }
+        else
+        {
+            data.open("database.txt", ios::app | ios::out);
+            data << " " << pId << " " << pName << " " << pPrice << " " << pNumbers << "\n";
+            data.close();
+        };
+    };
+    cout << "Success adding products"
 };
